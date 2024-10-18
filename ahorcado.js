bafbd41b26e4de507e7e5f660dacae72fe5c0e3b
gerberth92 <gerberthcontreras@gmail.com>
2024-10-17 19:22:40 -0500

    mejoras

--- ahorcado.js
+++ ahorcado.js
@@ -1,75 +1,97 @@
-var iniciarJuego = document.querySelector("#iniciar-juego");
-var comprobarPalabra = document.querySelector("#nueva-palabra");
-var palabraIngresada = document.querySelector("#input-nueva-palabra");
-var guiones = document.querySelector("#guiones");
-var letraErrada = document.querySelector("#letra-errada");
-var palabras = ["vaca","perro","gato","alacran","rata","gallina","condor",
-               "iguana","cocodrilo","caiman","paloma","gaviota","ballena",
-               "tiburon","delfin","canguro","colibri","mosca","camello",
-               "caballo","cangrejo","conejo","hipopotamo","mariposa"];
-var palabraOculta = "";
-var palabraAdivinada = "";
-var vidas = 4;
-comprobarPalabra.addEventListener("click" , comprobar);
-iniciarJuego.addEventListener("click" , iniciar);
+let ganaste = new Audio('ganaste.mp3');
+let perdiste = new Audio('perdiste.mp3');
+let correcto = new Audio('correcto.mp3');
+let error = new Audio('error.mp3');
+
+let iniciarJuego = document.getElementById("iniciar-juego");
+let letraErrada = document.getElementById("letra-errada");
+let guiones = document.getElementById("guiones");
+let input = document.getElementById("input");
+let comparar = document.getElementById("comparar");
+let palabraOculta = "";
+let gui = '';
+let vidas = 4;
+
+const palabras = ["vaca", "perro", "gato", "alacran", "rata", "gallina", "condor",
+  "iguana", "cocodrilo", "caiman", "paloma", "gaviota", "ballena",
+  "tiburon", "delfin", "canguro", "colibri", "mosca", "camello",
+  "caballo", "cangrejo", "conejo", "hipopotamo", "mariposa"];
+
+iniciarJuego.addEventListener("click", iniciar);
+comparar.addEventListener("click", comprobar);
 
 function iniciar() {
-
-    palabraOculta = palabras[Math.floor(Math.random()*palabras.length)];
-
-    for (var i = 0 ; i < palabraOculta.length ; i++) {
-
-        palabraAdivinada = palabraAdivinada + "_ ";
-    }
-        guiones.innerHTML = palabraAdivinada;
+  if (palabraOculta !== gui.split(' ').join('') & vidas > 0) {
+    alert('El juego ha iniciado');
+  } else if (gui.split(' ').join('') === palabraOculta && palabraOculta != '' || vidas === 0) {
+    reiniciar();
+    iniciar();
+  } else {
+    palabraOculta = palabras[Math.floor(Math.random() * palabras.length)].toUpperCase();
+    gui = palabraOculta.split('').map(() => '_').join(' ');
+    guiones.innerHTML = gui;
+    comparar.style.display = 'inline';
+    input.disabled = false;
+    input.focus();
+    dibujar();
+  }
 }
 
-function comprobar () {
+function comprobar() {
+  let letra = input.value.toUpperCase();
+  let almacen = gui.split(' ');
 
-    var letra = palabraIngresada.value.toUpperCase();
-    palabraOculta = palabraOculta.toUpperCase();
-    var nuevo = "";
-    var almacen = "";
-
-    for (var i = 0 ; i < palabraOculta.length ; i++) {
-
-        if (letra == palabraOculta[i]) {
-
-            nuevo = nuevo + letra + " ";
+  if (input.value !== '' && /^[A-Za-z]$/.test(input.value)) {
+    if (!palabraOculta.includes(letra)) {
+      error.play();
+      vidas --;
+      if (vidas === 0) {
+        setTimeout(() => {
+          perdiste.play();
+          guiones.innerHTML = palabraOculta;
+        }, 0);
+        setTimeout(() => {
+          alert('Has perdido');
+        }, 150);
+        comparar.style.display = 'none';
+      } else {
+        letraErrada.innerHTML = letra;
+      }
+    } else {
+      correcto.play();
+      for (let i = 0; i < palabraOculta.length; i++) {
+        if (letra === palabraOculta[i]) {
+          almacen[i] = letra;
         }
-        else {
-
-            nuevo = nuevo + palabraAdivinada [i*2] + " ";
-        }
+      }
     }
-
-    if (letra != palabraOculta[i]) {
-
-        almacen = letra + " ";
-        letraErrada.innerHTML = almacen;
+  
+    gui = almacen.join(' ');
+    guiones.innerHTML = gui;
+    input.value = '';
+    input.focus();
+  
+    if (gui.split(' ').join('') === palabraOculta) {
+      ganaste.play();
+      setTimeout(() => {
+        alert('Ganaste!!!');
+      }, 100);
+      comparar.style.display = 'none';
     }
-
-
-    if (nuevo == palabraAdivinada) {
-
-        vidas--;
-    }
-    
-    palabraAdivinada = nuevo;
-    guiones.innerHTML = palabraAdivinada;
-
-    if (vidas == 0) {
-
-        alert("Perdiste");
-    }
-
-    if (palabraAdivinada.search("_") ==-1) {
-
-        alert("Ganaste");
-    }
-
-    palabraIngresada.value = "";
-    palabraIngresada.focus();
 
     dibujar();
+  } else {
+    alert('Ingresa una letra');
+    input.value = '';
+    input.focus();
+  }
+}
+
+function reiniciar() {
+  vidas = 4;
+  palabraOculta = '';
+  gui = '';
+  guiones.innerHTML = '';
+  letraErrada.innerHTML = '';
+  pincel.clearRect(0, 0, pantalla.width, pantalla.height);
 }
--- canvas.js
+++ canvas.js
@@ -1,61 +1,48 @@
-var pantalla = document.querySelector("#ahorcado");
-var pincel = pantalla.getContext("2d");
+let pantalla = document.querySelector("#ahorcado");
+let pincel = pantalla.getContext("2d");
 
 function dibujar() {
+  pincel.strokeStyle = "black";
 
-    //esto dibuja la base del ahorcado
-    pincel.fillStyle = "black";
+  function dibujarLinea(x1, y1, x2, y2) {
+    pincel.lineWidth = 4;
     pincel.beginPath();
-    pincel.moveTo(450,350);
-    pincel.lineTo(550,350);
-    pincel.lineTo(500,350);
-    pincel.lineTo(500,60);
-    pincel.lineTo(700,60);
-    pincel.lineTo(700,80);
+    pincel.moveTo(x1, y1);
+    pincel.lineTo(x2, y2);
     pincel.stroke();
-    
-    //esto dibuja la cabeza
-    if (vidas <= 3) {
+  }
 
-        pincel.beginPath();
-        pincel.arc(700,100,20,0,2*Math.PI);
-        pincel.stroke();
-    }
+  // Dibuja la base del ahorcado
+  dibujarLinea(450, 350, 550, 350);
+  dibujarLinea(525, 352, 500, 300);
+  dibujarLinea(500, 300, 475, 352);
+  dibujarLinea(500, 350, 500, 54);
+  dibujarLinea(492, 60, 700, 55);
+  dibujarLinea(700, 55, 700, 80);
+  dibujarLinea(496, 105, 540, 58);
 
-    //esto dibuja el cuerpo
-    if (vidas <= 2) {
+  // Dibuja la cabeza
+  if (vidas <= 3) {
+    pincel.lineWidth = 4;
+    pincel.beginPath();
+    pincel.arc(700, 100, 20, 0, 2 * Math.PI);
+    pincel.stroke();
+  }
 
-        pincel.beginPath();
-        pincel.moveTo(700,120);
-        pincel.lineTo(700,200);
-        pincel.stroke();
-    }
+  // Dibuja el cuerpo
+  if (vidas <= 2) {
+    dibujarLinea(700, 120, 700, 200);
+  }
 
-    //esto dibuja los brazos
-    if (vidas <= 1) {
+  // Dibuja los brazos
+  if (vidas <= 1) {
+    dibujarLinea(700, 135, 670, 175);
+    dibujarLinea(700, 135, 730, 175);
+  }
 
-        pincel.beginPath();
-        pincel.moveTo(700,135);
-        pincel.lineTo(670,175);
-        pincel.stroke();
-
-        pincel.beginPath();
-        pincel.moveTo(700,135);
-        pincel.lineTo(730,175);
-        pincel.stroke();
-    }
-
-    //esto dibuja las piernas
-    if (vidas == 0) {
-
-        pincel.beginPath();
-        pincel.moveTo(700,200);
-        pincel.lineTo(670,250);
-        pincel.stroke();
-
-        pincel.beginPath();
-        pincel.moveTo(700,200);
-        pincel.lineTo(730,250);
-        pincel.stroke();
-    }
+  // Dibuja las piernas
+  if (vidas == 0) {
+    dibujarLinea(700, 200, 670, 250);
+    dibujarLinea(700, 200, 730, 250);
+  }
 }
